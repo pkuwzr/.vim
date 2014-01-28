ff09f1ace7edea97e01f1c0f35320fbeb5a4ad6c
Jerry King <jerry@localhost.localdomain>
2014-01-28 23:17:14 +0800

    Initial setup

---
+++ template/tpl.py
@@ -0,0 +1,15 @@
+##################################################
+#												 
+#	Author:<T_AUTHOR>,<<T_AUTHOR_EMAIL>>		
+#												 
+#	File:<T_FILENAME>							 
+#	Create Date:<T_CREATE_DATE>					 
+#												 
+##################################################
+
+#!/usr/bin/env python
+#encoding: utf-8
+
+<T_CURSOR>
+
+if __name__=="__main__":
